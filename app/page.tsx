70a9ba5fb2d449c218163a2ce30addd213ed3caa
EHWkddl <hyominj1014@gmail.com>
2025-12-10 07:03:22 +0900

    마지막

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,16 @@
 import TopicsList from '@/components/TopicsList'
-
-function getBaseUrl() {
-  // Client 환경
-  if (typeof window !== 'undefined') {
-    return window.location.origin
-  }
-
-  // Server 환경 (Vercel)
-  if (process.env.VERCEL_URL) {
-    return `https://${process.env.VERCEL_URL}`
-  }
-
-  // Dev 환경
-  return 'http://localhost:3000'
-}
-
-async function getTopics() {
-  const baseUrl = getBaseUrl()
-
-  const res = await fetch(`${baseUrl}/api/topics`, {
-    cache: 'no-store',
-  })
-
-  return res.json()
-}
+import connectMongoDB from '@/libs/mongodb'
+import Topic from '@/models/topic'
 
 export default async function Home() {
-  const { topics } = await getTopics()
+  await connectMongoDB()
+  const topics = await Topic.find().lean()
 
   return (
     <div className="mt-8">
       <h1 className="text-3xl font-bold my-1">WebDev Topics</h1>
       <p className="mb-8">MongoDB CRUD examples</p>
-      <TopicsList topics={topics} />
+      <TopicsList topics={JSON.parse(JSON.stringify(topics))} />
     </div>
   )
 }
